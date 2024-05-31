c7f616ee295737631eee951c6fdbdc6910edc58f
cksghks89 <song960705@naver.com>
2024-05-31 16:12:24 +0900

    emaillist3 & guestbook3 finish

---
+++ guestbook3/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -0,0 +1,46 @@
+package com.poscodx.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poscodx.guestbook.repository.GuestbookRepository;
+import com.poscodx.guestbook.vo.GuestbookVo;
+
+@Controller
+@RequestMapping("/")
+public class GuestbookController {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+	
+	@RequestMapping("/")
+	public String list(Model model) {
+		List<GuestbookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list", list);
+		return "index";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String deleteform(Model model, String no) {
+		model.addAttribute("no", no);
+		return "delete";
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	public String delete(GuestbookVo vo) {
+		guestbookRepository.deleteByNoAndPassword(vo.getNo(), vo.getPassword());
+		return "redirect:/";
+	}
+	
+	
+}
